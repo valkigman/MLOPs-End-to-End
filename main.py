ecc9de849a9a0dccf4dfcf2613abcb72e9b9eeff
KIGMAN <kvale8@gmail.com>
2024-08-20 22:18:19 +0300

    Further configuration executed

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.MyMLProject import logger
+from src.MyMLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom log")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
