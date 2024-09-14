8b2b46efe8ef96ac547d3ed603e28b1ea47c2a40
KIGMAN <kvale8@gmail.com>
2024-09-15 00:31:40 +0300

    Modified configuration.py file

--- src/MyMLProject/config/configuration.py
+++ src/MyMLProject/config/configuration.py
@@ -29,3 +29,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=Config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
