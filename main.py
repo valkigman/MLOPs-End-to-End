d3c32d066318883b54cc9d77493141d4fdf282af
KIGMAN <kvale8@gmail.com>
2024-09-23 17:27:31 +0300

    Additions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.MyMLProject import logger
 from src.MyMLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.MyMLProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from src.MyMLProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -22,3 +22,13 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
