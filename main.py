78a33e2131b95ad63348ee9a99743f9d97da8072
KIGMAN <kvale8@gmail.com>
2024-09-27 07:01:09 +0300

    tweaked main.py file accordingly

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.MyMLProject import logger
 from src.MyMLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.MyMLProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.MyMLProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.MyMLProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -27,6 +28,16 @@ STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
